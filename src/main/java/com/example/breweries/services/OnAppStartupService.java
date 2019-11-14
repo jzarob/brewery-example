27980d0b203d175bf9305df96b2742961b36fe36
caseyokane-8451 <casey.okane8451@gmail.com>
2019-11-14 11:17:10 -0500

    refactor to use CSV reader and get proper state value

--- src/main/java/com/example/breweries/services/OnAppStartupService.java
+++ src/main/java/com/example/breweries/services/OnAppStartupService.java
@@ -2,10 +2,13 @@ package com.example.breweries.services;
 
 import com.example.breweries.entity.Brewery;
 import com.example.breweries.repositories.BreweryRepository;
+import com.opencsv.CSVReader;
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -25,20 +28,19 @@ public class OnAppStartupService implements ApplicationListener<ApplicationReady
 
     try(InputStream inputStream = new ClassPathResource("breweries_us.csv").getInputStream();
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+        CSVReader csvReader = new CSVReader(inputStreamReader)) {
 
-      String line = bufferedReader.readLine(); // gets rid of header row
+      String[] line = csvReader.readNext(); // gets rid of header row
 
-      while((line = bufferedReader.readLine()) != null) {
+      while((line = csvReader.readNext()) != null) {
         //brewery_name,type,address,website,state,state_breweries
-        String[] values = line.split(",");
 
         Brewery brewery = new Brewery();
 
-        brewery.setName(values[0]);
-        brewery.setType(values[1]);
-        brewery.setAddress(values[2]);
-        brewery.setState(values[5]);
+        brewery.setName(line[0]);
+        brewery.setType(line[1]);
+        brewery.setAddress(line[2]);
+        brewery.setState(line[4]);
 
         breweries.add(brewery);
       }
